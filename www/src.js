36dd54f3b7550a4aa4c87c846b6ca7b18dea3fc6
trizeta <michele.rigo@trizeta.com>
2014-10-28 19:22:22 +0100

    add plugin

--- www/src.js
+++ www/src.js
@@ -436,8 +436,41 @@ require([
             //Attivo il push notification
             try{
                 pushNotification = window.plugins.pushNotification;
+                
+                if ( device.platform == 'android' || device.platform == 'Android' || device.platform == "amazon-fireos" ){
+                    pushNotification.register(
+                    successHandler,
+                    errorHandler,
+                    {
+                        "senderID":"1035210567078",
+                        "ecb":"onNotification"
+                    });
+                } else if ( device.platform == 'blackberry10'){
+                    pushNotification.register(
+                    successHandler,
+                    errorHandler,
+                    {
+                        invokeTargetId : "replace_with_invoke_target_id",
+                        appId: "replace_with_app_id",
+                        ppgUrl:"replace_with_ppg_url", //remove for BES pushes
+                        ecb: "pushNotificationHandler",
+                        simChangeCallback: replace_with_simChange_callback,
+                        pushTransportReadyCallback: replace_with_pushTransportReady_callback,
+                        launchApplicationOnPush: true
+                    });
+                } else {
+                    pushNotification.register(
+                    tokenHandler,
+                    errorHandler,
+                    {
+                        "badge":"true",
+                        "sound":"true",
+                        "alert":"true",
+                        "ecb":"onNotificationAPN"
+                    });
+                }
             }catch(e){
-            
+                errorlog("ERROR PUSH NOTIFICATION!!!",e);
             }
             try{
                 //Recupero id del dispositivo
@@ -484,6 +517,96 @@ require([
                     
            
         };
+
+        // result contains any message sent from the plugin call
+        function successHandler (result) {
+            alert('result = ' + result);
+        };
+
+
+        function errorHandler (error) {
+            alert('error = ' + error);
+        };
+
+        function onNotificationAPN (event) {
+            if ( event.alert )
+            {
+                navigator.notification.alert(event.alert);
+            }
+
+            if ( event.sound )
+            {
+                var snd = new Media(event.sound);
+                snd.play();
+            }
+
+            if ( event.badge )
+            {
+                pushNotification.setApplicationIconBadgeNumber(successHandler, errorHandler, event.badge);
+            }
+        }
+        
+        function onNotification(e) {
+            alert('<li>EVENT -> RECEIVED:' + e.event + '</li>');
+
+            switch( e.event )
+            {
+            case 'registered':
+                if ( e.regid.length > 0 )
+                {
+                    alert('<li>REGISTERED -> REGID:' + e.regid + "</li>");
+                    // Your GCM push server needs to know the regID before it can push to this device
+                    // here is where you might want to send it the regID for later use.
+                    console.log("regID = " + e.regid);
+                }
+            break;
+
+            case 'message':
+                // if this flag is set, this notification happened while we were in the foreground.
+                // you might want to play a sound to get the user's attention, throw up a dialog, etc.
+                if ( e.foreground )
+                {
+                    alert('<li>--INLINE NOTIFICATION--' + '</li>');
+
+                    // on Android soundname is outside the payload.
+                    // On Amazon FireOS all custom attributes are contained within payload
+                    var soundfile = e.soundname || e.payload.sound;
+                    // if the notification contains a soundname, play it.
+                    var my_media = new Media("/android_asset/www/"+ soundfile);
+                    my_media.play();
+                }
+                else
+                {  // otherwise we were launched because the user touched a notification in the notification tray.
+                    if ( e.coldstart )
+                    {
+                        alert('<li>--COLDSTART NOTIFICATION--' + '</li>');
+                    }
+                    else
+                    {
+                        alert('<li>--BACKGROUND NOTIFICATION--' + '</li>');
+                    }
+                }
+
+               alert('<li>MESSAGE -> MSG: ' + e.payload.message + '</li>');
+                   //Only works for GCM
+               alert('<li>MESSAGE -> MSGCNT: ' + e.payload.msgcnt + '</li>');
+               //Only works on Amazon Fire OS
+               alert('<li>MESSAGE -> TIME: ' + e.payload.timeStamp + '</li>');
+            break;
+
+            case 'error':
+                alert('<li>ERROR -> MSG:' + e.msg + '</li>');
+            break;
+
+            default:
+                alert('<li>EVENT -> Unknown, an event was received and we do not know what it is</li>');
+            break;
+          }
+        }
+
+
+        /***************************************************************************************************************************/
+
     
         onBackKeyDown = function(e){
            createConfirmation("Sei sicuro di uscire da Shooopit?", 
